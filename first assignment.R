d5f37f1b7bee4168ce0bd4cfbfe114d008cf136f
vipinsoma <42753176+vipinsoma@users.noreply.github.com>
2018-09-10 18:53:22 -0400

    Update first assignment.R

--- first assignment.R
+++ first assignment.R
@@ -2,3 +2,24 @@ getwd()
 q2<-read.csv('qa1.csv',TRUE)
 q2
 plot(q1$Age,q1$BF..,xlab='Age',ylab = 'Body Fat',main = ' AgevsBF%')
+
+//#table is shown below 
+Age	BF %
+23	9.5
+23	26.5
+27	7.8
+27	17.8
+39	31.4
+41	25.9
+47	27.4
+9	27.2
+50	31.2
+52	34.6
+54	42.5
+56	28.8
+57	33.4
+58	30.2
+58	34.1
+60	32.9
+61	35.7
+//
